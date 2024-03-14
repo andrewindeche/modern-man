cf411230ad208a1d062d584efab903c27a7a5851
AndrewIndeche <indecheandrew@outlook.com>
2024-03-14 16:41:11 +0300

    add Pillow to dependencies and update migrations

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Customer(AbstractUser):
@@ -17,6 +17,19 @@ class Customer(AbstractUser):
     )
     payment_method = models.CharField(_('Payment Method'), max_length=20, choices=PAYMENT_METHOD_CHOICES)
     
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name=_('groups'),
+        blank=True,
+        related_name='customers'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='customers'
+    )
+    
     def __str__(self):
         return self.username
     
@@ -27,7 +40,7 @@ class Category(models.Model):
         ('neckwear', 'Neckwear & Accessories'),
         ('shoes', 'Shoes')
     ]
-    name = models.CharField(max_lenght=100, choices=CATEGORY_CHOICES)
+    name = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     def __str__(self):
         return self.get_name_display()
     
@@ -53,4 +66,25 @@ class Order(models.Model):
     
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Order Item #{self.pk} for {self.order}"
+    
+class Cart(models.Model):
+    user = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    items = models.ManyToManyField('Product', through='CartItem')
+
+    def __str__(self):
+        return f"Shopping Cart for {self.user.username}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"CartItem for {self.product.name} in {self.cart}"
