dda114a62365be63ebbd0da9bc8145f90b7998f9
AndrewIndeche <indecheandrew@outlook.com>
2024-03-14 10:59:45 +0300

    add dotenv to dependencies

--- backend/modernman/settings.py
+++ backend/modernman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--3fjv4$d*ke840#l6ma$@2h&wwa5%7&cb8u44x0&xyk76hpm&!'
+SECRET_KEY = 'django-insecure-b)*v3my@t)(*szfi@zp0zf1^$(9z*vida6#w_!=sz6ead3hb8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
