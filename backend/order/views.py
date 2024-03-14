3f589da631da8b54902d5d6219f23e4a7efc3f0a
AndrewIndeche <indecheandrew@outlook.com>
2024-03-14 22:54:55 +0300

    add views for Product,Order and Cart

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import path
+from .models import Product, Order, Cart
 
 # Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+
+class AddToCartView(CreateView):
+    model = Cart
+    fields = ['products']
+    template_name = 'add_to_cart.html'
+    success_url = '/cart/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class CheckoutView(CreateView):
+    model = Order
+    fields = ['products']
+    template_name = 'checkout.html'
+    success_url = '/thank-you/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.is_ordered = True
+        return super().form_valid(form)
