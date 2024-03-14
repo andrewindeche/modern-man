48abb4ea89fe21afa283c643322611a9e41dc895
AndrewIndeche <indecheandrew@outlook.com>
2024-03-14 15:11:34 +0300

    register models in admin

--- backend/order/models.py
+++ backend/order/models.py
@@ -30,6 +30,16 @@ class Category(models.Model):
     name = models.CharField(max_lenght=100, choices=CATEGORY_CHOICES)
     def __str__(self):
         return self.get_name_display()
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     user = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -41,3 +51,6 @@ class Order(models.Model):
     def __str__(self):
         return f"Order #{self.pk}"
     
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
