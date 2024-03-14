09a1a5267be64126b4fdabe8a69ce28be998b4df
AndrewIndeche <indecheandrew@outlook.com>
2024-03-14 20:41:31 +0300

    update Product model

--- backend/order/models.py
+++ backend/order/models.py
@@ -50,6 +50,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    added_by_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
