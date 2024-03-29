dd756c1ee0a6d3c99f641722bb5a91dfe9f7fb33
AndrewIndeche <indecheandrew@outlook.com>
2024-03-29 17:07:51 +0300

    update favorite position

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -9,29 +9,29 @@ const Modal = () => {
         <div className="Modal">
             <div className="modalheader">
                 <h2>Price: $1000</h2>
-                <div className="topheadericon">{[...Array(4)].map((_, index) => (
-                <FontAwesomeIcon key={index} icon={faStar} className="star" size="2x" /> ))}</div>
-                <FontAwesomeIcon icon={faHeart} className="favorite" size="2x" />
+                <div className="topheadericon"><p></p>{[...Array(4)].map((_, index) => (
+                <FontAwesomeIcon key={index} icon={faStar} className="star" size="1x" /> ))}</div>
             </div>
             <div className="modalbody">
             <img src={Suit} id="posterImage" alt="suitimage" />
-                <div>
+                    <div>
                     <p className="title">Black Full Italian Men's Tuxedo</p>
-                    <p>Full Italian Men's suit consisting of:
+                    <p>Full Italian Men's suit:
                         <ol className="productdescription">
-                        <li> A white XL sized long sleeved shirt.</li>
+                        <li> White XL sized long sleeved shirt.</li>
                         <li>Long black neck tie</li>
-                        <li>black long sleeved coat with a pocket on the left side.</li>
-                        <li>white pocket square</li>
-                        <li>neck tie holder</li>
-                        <li>size 34 black trouser</li>
-                        <li>black flower ribbon</li>
-                        <li>black xxl half coat</li>
+                        <li>Black long sleeved coat with a pocket on the left side.</li>
+                        <li>White pocket square</li>
+                        <li>Neck tie holder</li>
+                        <li>Size 34 black trouser</li>
+                        <li>Black flower ribbon</li>
+                        <li>Black xxl half coat</li>
                         </ol>
                         </p>
                         <div className="modalicons">
-                        <FontAwesomeIcon icon={faShoppingCart} className="shoppingcarticon" size="2x" />
-                        <FontAwesomeIcon icon={faWindowClose} className="windowclose"  size="2x"/>
+                        <FontAwesomeIcon icon={faShoppingCart} className="shoppingcarticon" size="1x" />
+                        <FontAwesomeIcon icon={faWindowClose} className="windowclose"  size="1x"/>
+                        <FontAwesomeIcon icon={faHeart} className="favorite" size="1x" />
                         </div>
                 </div>
             </div>
