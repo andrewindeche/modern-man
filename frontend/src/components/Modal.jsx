4a609a299c943fc6ff84b0de5c5b9b5fc1d50933
AndrewIndeche <indecheandrew@outlook.com>
2024-04-01 11:56:42 +0300

    update style, add previews

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faStar, faWindowClose, faShoppingCart } from '@fortawesome/free-solid-svg-icons'; 
 import Suit from '../images/tuxedo1.jpg';
+import Preview1 from '../images/black lapel coat.jpg';
+import Preview2 from '../images/black tie.jpg';
+import Preview3 from '../images/white shirt.jpg';
+import Preview4 from '../images/halfcoat.jpg';
+import Preview5 from '../images/tuxedo trouser.jpg';
+import Preview6 from '../images/pocket square.jpg';
 
 const Modal = () => {
     return (
@@ -16,7 +22,6 @@ const Modal = () => {
             <img src={Suit} id="posterImage" alt="suitimage" />
                     <div>
                     <p className="title">Price: $1000</p>
-                    <p>
                         <ol className="productdescription">
                         <li> White XL sized long sleeved shirt.</li>
                         <li>Long black neck tie</li>
@@ -27,7 +32,14 @@ const Modal = () => {
                         <li>Black flower ribbon</li>
                         <li>Black xxl half coat</li>
                         </ol>
-                        </p>
+                        <div className="previews">
+                        <img src={Preview1} id="previewImage" alt="black lapel coat" />
+                        <img src={Preview2} id="previewImage" alt="black tie" />
+                        <img src={Preview3} id="previewImage" alt="Blue Tux" />
+                        <img src={Preview4} id="previewImage" alt="halfcoat" />
+                        <img src={Preview5} id="previewImage" alt="neckwear" />
+                        <img src={Preview6} id="previewImage" alt="White pocket square" />
+                        </div>
                         <div className="modalicons">
                         <FontAwesomeIcon icon={faShoppingCart} className="shoppingcarticon" size="1x" />
                         <FontAwesomeIcon icon={faWindowClose} className="windowclose"  size="1x"/>
