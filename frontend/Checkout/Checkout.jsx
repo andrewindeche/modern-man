71a6d29b5b66d8ee9429447d4c0b15cc50f7a447
AndrewIndeche <indecheandrew@outlook.com>
2024-04-02 17:01:49 +0300

    style personal information card

--- frontend/Checkout/Checkout.jsx
+++ frontend/Checkout/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NotificationBar from '../src/components/NotificationBar';
 import SearchBar from '../src/components/SearchBar';
+import NavButtons from '../src/components/NavButtons';
 import Mpesa from '../src/images/mastercard.png'
 import Visa from '../src/images/mpesa.png'
 import Mastercard from '../src/images/paypal.png'
@@ -11,29 +12,30 @@ const Checkout = () => {
         <>
           <NotificationBar />
           <SearchBar />
+          <NavButtons />
           <div className='checkout'>
+          <h1>Check Out</h1>
           <div className='personaldetails'>
-          <h1 className='checkout-header'>Check Out</h1>
           <h4 className='checkouttitle'>Personal Information</h4>
             <ul className='aboutuser'>
-            <h4 className='checkouttitle'>About</h4>
+            <p className='about'>About</p>
               <li>Andrew Indeche</li>
               <li>Westlands</li>
               <li>Nairobi, Kenya </li>
-              <li>Indecheandrew@gmail.com</li>
+              <li id='email'>Indecheandrew@gmail.com</li>
             </ul>
           <div className='payments'>
-          <h4 className='checkouttitle'>Select Payment</h4>
+          <p>Select Payment</p>
           <img src={Mpesa} alt="Mpesa" />
           <img src={Visa} alt="Visa" />
-          <img src={Mastercard} alt="Mastercard" />
           <img src={Paypal} alt="Paypal" />
+          <img src={Mastercard} alt="Mastercard" />
           </div>
           <div className='paymentselected'>
           <h4 className='checkouttitle'>Payment Selected</h4>
           <img src={Mpesa} alt="Blue Tuxedo" />
           </div>
-        </div>
+          </div>
       </div>
         </>
     )
