52239c855694e5027c615ed43cb3043349eed69b
AndrewIndeche <indecheandrew@outlook.com>
2024-04-03 17:02:32 +0300

    create checkout amount container

--- frontend/Checkout/Checkout.jsx
+++ frontend/Checkout/Checkout.jsx
@@ -7,7 +7,6 @@ import Visa from '../src/images/mpesa.png';
 import Mastercard from '../src/images/paypal.png';
 import Paypal from '../src/images/visa.png';
 import Suit1 from '../src/images/tuxedo1.jpg';
-import Suit2 from '../src/images/tuxedo4.jpg';
 import Suit3 from '../src/images/white tux.jpg';
 
 const Checkout = () => {
@@ -44,12 +43,20 @@ const Checkout = () => {
             <span className='checkoutitem'><img src={Suit1} alt="tuxedo"/>
             <p>Black Full Italian Men's Tuxedo * 2 </p>
             <a>$2000</a></span>
-            <span className='checkoutitem'><img src={Suit2} alt="tuxedo" />
+            <span className='checkoutitem'><img src={Suit1} alt="tuxedo" />
             <p>Grey full Official Men's Suit * 1</p>
             <a>$1500</a></span>
             <span className='checkoutitem'><img src={Suit3} alt="tuxedo" />
             <p>White full Official Men's Suit* 1</p>
             <a>$1800</a></span>
+            </div>
+            <div className='grandtotal'>
+              <span className='checkoutamount'><p>SubTotal</p>
+              <a>$3400</a></span>
+              <span className='checkoutamount'><p>Shipping</p>
+              <a>$0</a></span>
+              <span className='checkoutamount'><p>Total</p>
+              <a>$3400</a></span>
             </div>
         </div>
         </>
