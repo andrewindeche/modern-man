7f06bfaacabf69a08d041765a1535af3258e6463
AndrewIndeche <indecheandrew@outlook.com>
2024-04-03 16:52:02 +0300

    add checkout cart

--- frontend/Checkout/Checkout.jsx
+++ frontend/Checkout/Checkout.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import NotificationBar from '../src/components/NotificationBar';
 import SearchBar from '../src/components/SearchBar';
 import NavButtons from '../src/components/NavButtons';
-import Mpesa from '../src/images/mastercard.png'
-import Visa from '../src/images/mpesa.png'
-import Mastercard from '../src/images/paypal.png'
-import Paypal from '../src/images/visa.png'
+import Mpesa from '../src/images/mastercard.png';
+import Visa from '../src/images/mpesa.png';
+import Mastercard from '../src/images/paypal.png';
+import Paypal from '../src/images/visa.png';
+import Suit1 from '../src/images/tuxedo1.jpg';
+import Suit2 from '../src/images/tuxedo4.jpg';
+import Suit3 from '../src/images/white tux.jpg';
 
 const Checkout = () => {
     return(
@@ -36,6 +39,18 @@ const Checkout = () => {
           <img src={Mpesa} alt="Blue Tuxedo" />
             </div>
           </div>
+          <div className="cartdetails">
+            <h4>Shopping Cart Details</h4>
+            <span className='checkoutitem'><img src={Suit1} alt="tuxedo"/>
+            <p>Black Full Italian Men's Tuxedo * 2 </p>
+            <a>$2000</a></span>
+            <span className='checkoutitem'><img src={Suit2} alt="tuxedo" />
+            <p>Grey full Official Men's Suit * 1</p>
+            <a>$1500</a></span>
+            <span className='checkoutitem'><img src={Suit3} alt="tuxedo" />
+            <p>White full Official Men's Suit* 1</p>
+            <a>$1800</a></span>
+            </div>
         </div>
         </>
     )
