1799effdbf2c5725b0349211ef357d6db9d65a5c
AndrewIndeche <indecheandrew@outlook.com>
2024-04-04 15:43:01 +0300

    update checkout style

--- frontend/Checkout/Checkout.jsx
+++ frontend/Checkout/Checkout.jsx
@@ -10,34 +10,35 @@ import Suit1 from '../src/images/tuxedo1.jpg';
 import Suit3 from '../src/images/white tux.jpg';
 
 const Checkout = () => {
-    return(
-        <>
-          <NotificationBar />
-          <SearchBar />
-          <NavButtons />
-          <div className='checkout'>
-          <h1 id="Checkout">Check Out</h1>
-          <div className='personaldetails'>
-          <h4 className='checkouttitle'>Personal Information</h4>
-            <ul className='aboutuser'>
-            <p className='about'>About</p>
-              <li>Andrew Indeche</li>
-              <li>Westlands</li>
-              <li>Nairobi, Kenya </li>
-              <li id='email'>Indecheandrew@gmail.com</li>
+  return(
+    <>
+      <NotificationBar />
+      <SearchBar />
+      <NavButtons />
+      <div className='checkout'>
+        <h1 id="Checkout">Check Out</h1>
+        <div className='personaldetails'>
+        <h4 className='checkouttitle'>Personal Information</h4>
+          <ul className='aboutuser'>
+          <p className='about'>About</p>
+            <li>Andrew Indeche</li>
+            <li>Westlands</li>
+            <li>Nairobi, Kenya </li>
+            <li id='email'>Indecheandrew@gmail.com</li>
             </ul>
-          <div className='payments'>
+        <div className='payments'>
           <p>Select Payment</p>
           <img src={Mpesa} alt="Mpesa" />
           <img src={Visa} alt="Visa" />
           <img src={Paypal} alt="Paypal" />
           <img src={Mastercard} alt="Mastercard" />
-          </div>
-          <div className='paymentselected'>
+            </div>
+        <div className='paymentselected'>
           <h4 className='checkouttitle'>Payment Selected</h4>
           <img src={Mpesa} alt="Blue Tuxedo" />
             </div>
-          </div>
+        </div>
+        <div className='checkoutdetails'>
           <div className="cartdetails">
             <h4>Shopping Cart Details</h4>
             <span className='checkoutitem'><img src={Suit1} alt="tuxedo"/>
@@ -56,8 +57,11 @@ const Checkout = () => {
               <span className='checkoutamount'><p>Shipping</p>
               <a>$0</a></span>
               <span className='checkoutamount'><p>Total</p>
-              <a>$3400</a></span>
+              <a>$3400</a>
+              </span>
+              <span className='checkoutamount'><button>Place Order</button></span>
             </div>
+          </div>
         </div>
         </>
     )
