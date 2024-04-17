5a6d8b6891d64678b80d434e7ecde4de56911e39
AndrewIndeche <indecheandrew@outlook.com>
2024-04-17 13:02:15 +0300

    update cover images style

--- frontend/App/App.jsx
+++ frontend/App/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import "../style.css";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Homepage from "../Homepage/HomePage";
-
-const router = createBrowserRouter([
-    {
-    path: '/',
-    element: <Homepage />,
-    },
-    ]
- );
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from "../Homepage/HomePage.jsx";
+import Registration from "../Registration/Registration.jsx";
+import Login from "../Login/Login.jsx";
+import Forgot from "../Forgot/Forgot.jsx";
+import SearchPage from "../Searchpage/SearchPage.jsx";
+import Checkout from "../Checkout/Checkout.jsx"
 
 const App = () => {
-    return(
-        <RouterProvider router={router} />
-    );
-  };
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/registration" element={<Registration />} />
+                <Route path="/forgot" element={<Forgot />} />
+                <Route path="/searchpage" element={<SearchPage />} />
+                <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+    </Router>
+        );
+    };
 export default App;
