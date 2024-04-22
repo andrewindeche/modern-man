e7b4f66dbf7068eec85c8fd1ab36e44f768f42f1
AndrewIndeche <indecheandrew@outlook.com>
2024-04-22 17:53:30 +0300

    update variables

--- backend/modernman/settings.py
+++ backend/modernman/settings.py
@@ -24,12 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = os.getenv('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -89,6 +85,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432'),
+        'SECRET_KEY': os.getenv('SECRET_KEY')
     }
 }
 
