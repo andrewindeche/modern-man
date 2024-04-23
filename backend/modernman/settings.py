e7c13a1f661bf7f21fd59d980244e12c52515398
AndrewIndeche <indecheandrew@outlook.com>
2024-04-23 17:36:08 +0300

    update alternative key value for host

--- backend/modernman/settings.py
+++ backend/modernman/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME','ecommerce'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST','db'),
+        'HOST': os.getenv('DB_HOST','localhost'),
         'PORT': os.getenv('DB_PORT'),
     }
 }
