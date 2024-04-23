d4f6719a6026d137f47b854b24327e25f0fbbc02
AndrewIndeche <indecheandrew@outlook.com>
2024-04-23 17:38:10 +0300

    update alternative user

--- backend/modernman/settings.py
+++ backend/modernman/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
       'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME','ecommerce'),
-        'USER': os.getenv('DB_USER'),
+        'USER': os.getenv('DB_USER','circleci'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST','localhost'),
         'PORT': os.getenv('DB_PORT'),
