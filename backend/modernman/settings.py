4f5eaef51b97aa7bdf53cdde87ed48faab00e470
AndrewIndeche <indecheandrew@outlook.com>
2024-05-16 08:56:38 +0300

    create dump files

--- backend/modernman/settings.py
+++ backend/modernman/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
