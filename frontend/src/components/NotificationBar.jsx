546ffaceb93d6f0e8a7a2eb0acdd6f3c3a61b846
AndrewIndeche <indecheandrew@outlook.com>
2024-05-19 16:40:07 +0300

    update NotificationBar navigation

--- frontend/src/components/NotificationBar.jsx
+++ frontend/src/components/NotificationBar.jsx
@@ -1,14 +1,19 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import shoes from '../images/shoes.webp';
 
 const NotificationBar = () => {
+    const navigate = useNavigate();
+
+    const handleLearnMoreClick = () => {
+        navigate('/searchpage?discounted=true');
+    };
+
     return (
         <div className="notificationbar">
             <img src={shoes} alt="My Shoes" className='shoes' />
             <p>Get 50% off Selected Shoes</p>
-            <Link to="/searchpage?discounted=true">
-                <button id="notificationbutton">Learn More</button>
-            </Link>
+            <button id="notificationbutton" onClick={handleLearnMoreClick}>Learn More</button>
         </div>
     );
 }
