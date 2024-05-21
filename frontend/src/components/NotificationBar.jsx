31da97df0b4758cc4770f0a403437a909ab4ab80
AndrewIndeche <indecheandrew@outlook.com>
2024-05-21 13:38:41 +0300

    add imports for slice and keyframes

--- frontend/src/components/NotificationBar.jsx
+++ frontend/src/components/NotificationBar.jsx
@@ -1,61 +1,24 @@
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDiscountedProducts } from '../store/discountsSlice';
 import { css, keyframes } from '@emotion/react';
-
-
-const fadeInOut = keyframes`
-  0%, 100% { opacity: 0; }
-  50% { opacity: 1; }
-`;
+import shoes from '../images/shoes.webp';
 
 const NotificationBar = () => {
-    const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { items: discountedProducts, loading, error } = useSelector(state => state.discount);
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-        dispatch(fetchDiscountedProducts());
-    }, [dispatch]);
-
-    useEffect(() => {
-        if (discountedProducts.length > 0) {
-            const interval = setInterval(() => {
-                setCurrentIndex((prevIndex) => (prevIndex + 1) % discountedProducts.length);
-            }, 5000);
-            return () => clearInterval(interval);
-        }
-    }, [discountedProducts]);
 
     const handleLearnMoreClick = () => {
         navigate('/searchpage?discounted=true');
     };
 
-     return (
+    return (
         <div className="notificationbar">
-            {loading ? (
-                <p>Loading...</p>
-            ) : error ? (
-                <p>Error: {error}</p>
-            ) : discountedProducts.length > 0 ?  (
-                <div
-                css={css`
-                    animation: ${fadeInOut} 5s linear infinite;
-                    display: flex;
-                    flex-direction: column;
-                `}>  
-                <img src={discountedProducts[currentIndex].image} className='shoes' />
-                    <p> Get {discountedProducts[currentIndex].discount_percentage}% off on {discountedProducts[currentIndex].name}</p>
-                    <button id="notificationbutton" onClick={handleLearnMoreClick}>Learn More</button>
-                </div>
-            ) : (
-                <p>No discount available</p>
-            )}
+            <img src={shoes} alt="My Shoes" className='shoes' />
+            <p>Get 50% off Selected Shoes</p>
+            <button id="notificationbutton" onClick={handleLearnMoreClick}>Learn More</button>
         </div>
     );
 }
-
 
 export default NotificationBar;
