7d94e1995bdc735e77b2a404558336b886cf2ffa
AndrewIndeche <indecheandrew@outlook.com>
2024-05-21 11:49:38 +0300

    add css effect to NotificationBar

--- backend/modernman/settings.py
+++ backend/modernman/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 APPEND_SLASH = True
 
