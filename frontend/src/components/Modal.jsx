3fb4216c6e8da10b13a15142d1c5fc9bcada3059
AndrewIndeche <indecheandrew@outlook.com>
2024-05-24 08:56:39 +0300

    update Modal position style

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,32 +4,32 @@ import { faHeart, faStar, faWindowClose, faShoppingCart } from '@fortawesome/fre
 import Preview3 from '../images/white shirt.webp';
 import Preview5 from '../images/tuxedo trouser.webp';
 
-const ModalContent = ({ onClose }) => {
+const ModalContent = ({ item, onClose, position = { top: 0, left: 0 } }) => {
+      if (!item) return null;
+
+      const { name, image, price, description, rating } = item;
+      const modalStyle = {
+        top: position.top,
+        left: position.left,
+      };
   return (
     <>
       <div className="Modal">
         <div className="modalheader">
-          <h2>Black Full Italian Men's Tuxedo</h2>
+          <h2>{ name }</h2>
           <div className="topheaderstar">
-            {[...Array(4)].map((_, index) => (
+            {[...Array(rating)].map((_, index) => (
               <FontAwesomeIcon key={index} icon={faStar} className="star" size="2x" />
             ))}
           </div>
         </div>
         <div className="modalbody">
-          <img src={Preview3} id="posterImage" alt="suitimage" />
+          <img src={image}id="posterImage" alt={name} />
           <div>
-            <ol className="productdescription">
-              <p className="title">Price: $1000</p>
-              <li>White XL sized long sleeved shirt.</li>
-              <li>Long black neck tie</li>
-              <li>Black long sleeved coat.</li>
-              <li>White pocket square</li>
-              <li>Neck tie holder</li>
-              <li>Size 34 black trouser</li>
-              <li>Black flower ribbon</li>
-              <li>Black xxl half coat</li>
-            </ol>
+            <div className="productdescription">
+              <p className="title">Price: {price}</p>
+              {description}
+            </div>
             <div className="modalicons">
               <FontAwesomeIcon icon={faShoppingCart} className="shoppingcarticon" size="1x" />
               <span className="tooltip-text-modal">Checkout</span>
