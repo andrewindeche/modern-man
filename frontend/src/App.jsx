5d4ba3f30cdaa12114c218e0e07a872399ecc7ec
AndrewIndeche <indecheandrew@outlook.com>
2024-06-29 18:11:45 +0300

    protect routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import Homepage from './pages/HomePage';
@@ -9,16 +9,23 @@ import Forgot from './pages/Forgot';
 import SearchPage from './pages/SearchPage';
 import Checkout from './pages/Checkout';
 import ErrorPage from './pages/ErrorPage';
+import { AuthProvider } from './components/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Homepage />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/registration" element={<Registration />} />
-    <Route path="/forgot" element={<Forgot />} />
-    <Route path="/searchpage" element={<SearchPage />} />
-    <Route path="/checkout" element={<Checkout />} />
-    <Route path="*" element={<ErrorPage />} />
-  </Routes>
+  <AuthProvider>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/forgot" element={<Forgot />} />
+      <Route path="/searchpage" element={<SearchPage />} />
+      <Route
+        path="/checkout"
+        element={<ProtectedRoute><Checkout /></ProtectedRoute>}
+      />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </AuthProvider>
 );
 export default App;
